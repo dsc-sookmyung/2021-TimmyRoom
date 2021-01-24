52c0ba792ab3f7a07e9f78e1469106d89821ef0f
YEJIN-LILY <gristar98@naver.com>
2021-01-24 22:58:52 +0900

    conflict solved

--- imageResize.py
+++ imageResize.py
@@ -5,7 +5,7 @@ from PIL import Image
 files = glob.glob('/Users/siyeon/Desktop/DSC/timmyRoom/2021-TimmyRoom/image/데자와/*.jpg') # 각자 로컬에 맞게 변경
 
 print(files)
->>>>>>> 87b77e57de62de46fc640aa3e4f3e72097c82d1e
+
 for f in files:
     img = Image.open(f)
     img = img.convert("RGB")
