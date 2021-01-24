e618b9779e8abc013a976717dbe69d1d4e77e112
ksmself <53217988+ksmself@users.noreply.github.com>
2021-01-24 22:53:07 +0900

    pocari to jpg

--- imageResize.py
+++ imageResize.py
@@ -2,13 +2,13 @@ import os
 import glob
 from PIL import Image
 
-files = glob.glob('/Users/siyeon/Desktop/DSC/timmyRoom/2021-TimmyRoom/image/데자와/*.jpg') # 각자 로컬에 맞게 변경
+files = glob.glob(
+    'C:/Users/김승미/Desktop/숙명여자대학교/DSC/timmyRoom/2021-TimmyRoom/image/pocari/*.jpg')  # 각자 로컬에 맞게 변경
 print(files)
 for f in files:
     img = Image.open(f)
     img = img.convert("RGB")
     img_resize = img.resize((150, 150), Image.ANTIALIAS)
-    
-    
+
     title, ext = os.path.splitext(f)
     img_resize.save(title + '_change' + ext)
