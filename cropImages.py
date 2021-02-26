0f9036cef5524431f185a6e8a50b1a644d4b2753
tldusqkr <tldusqkr@gmail.com>
2021-02-26 16:26:45 +0900

    Update cropImages.py

--- cropImages.py
+++ cropImages.py
@@ -7,13 +7,13 @@ print(files)
 
 count = -1
 for f in files:
-    count += 1
     img = cv2.imread(f)
     x,y,w,h	= cv2.selectROI('img', img, False)
     if w and h:
         roi = img[y:y+h, x:x+w]
         cv2.imshow('cropped', roi)  # ROI 지정 영역을 새창으로 표시
         cv2.moveWindow('cropped', 0, 0) # 새창을 화면 좌측 상단에 이동
+        count += 1
         cv2.imwrite('./crop/'+str(count)+'.jpg', roi)   # ROI 영역만 파일로 저장
 
     cv2.waitKey(0)
