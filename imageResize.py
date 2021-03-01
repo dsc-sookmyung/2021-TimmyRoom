7e08aa03d99a8a20665a0d4477b55ee0ad7ddade
tldusqkr <tldusqkr@gmail.com>
2021-03-01 22:09:48 +0900

    Update imageResize.py
    저장위치 변경

--- imageResize.py
+++ imageResize.py
@@ -2,17 +2,16 @@ import os
 import glob
 from PIL import Image
 
-files = glob.glob('/Users/siyeon/Desktop/DSC/timmyRoom/핫식스/*.jpg') # 각자 로컬에 맞게 변경
+files = glob.glob('/Users/siyeon/Desktop/DSC/timmyRoom/2021-TimmyRoom/Image/image_crop/crop_식혜/*.jpg') # 각자 로컬에 맞게 변경
 
 print(files)
 
+count = 0
 for f in files:
     img = Image.open(f)
     img = img.convert("RGB")
     img_resize = img.resize((150, 150), Image.ANTIALIAS)
-    
-    
-    title, ext = os.path.splitext(f)
-  
-    img_resize.save(title +'changed_'+ ext)
+
+    img_resize.save('/Users/siyeon/Desktop/DSC/timmyRoom/resize/식혜/'+str(count)+'.jpg')
+    count += 1
     
