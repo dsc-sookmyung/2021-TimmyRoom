2007b2bec4545112466523975cdc9f7038b0c1f2
tldusqkr <tldusqkr@gmail.com>
2021-03-15 20:15:53 +0900

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -11,6 +11,7 @@ import Chat from './components/Chat';
 import Need from './components/Need'; 
 import ChatWithDev from './components/ChatWithDev'; 
 import WritePage from './components/WritePage';
+import ListPage from './components/ListPge';
 
 const App = () => {
     const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
@@ -24,14 +25,15 @@ const App = () => {
                 ) : (
                     <Route exact path="/" component={Auth(LogOutMain, false)} />
                 )} */}
-                <Route exact path="/" component={Auth(LogOutMain, false)} />
-                <Route exact path="/login" component={Auth(LogInPage, false)} />
-                <Route exact path="/signup" component={Auth(SignUpPage, false)} />
-                <Route exact path="/main" component={Auth(Main, true)} />
-                <Route exact path="/chat" component={Auth(Chat, true)} />
-                <Route exact path="/need" component={Auth(Need, true)} />
-                <Route exact path="/chatwithdev" component={Auth(ChatWithDev, true)} />
-                <Route exact path="/write" component={Auth(WritePage, true)} />
+                <Route exact path="/" component={Auth(LogOutMain, null)} />
+                <Route exact path="/login" component={Auth(LogInPage, null)} />
+                <Route exact path="/signup" component={Auth(SignUpPage, null)} />
+                <Route exact path="/main" component={Auth(Main, null)} />
+                <Route exact path="/chat" component={Auth(Chat, null)} />
+                <Route exact path="/need" component={Auth(Need, null)} />
+                <Route exact path="/chatwithdev" component={Auth(ChatWithDev, null)} />
+                <Route exact path="/write" component={Auth(WritePage, null)} />
+                <Route exact path="/list" component={Auth(ListPage, null)} />
                 <Route path="/*">404 Not Found</Route>
             </Switch>
         </Router>
